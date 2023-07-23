ece601bc070451a12c4268e54fa3216d0be53908
eric nichols <ebn646@gmail.com>
2023-07-23 14:53:03 -0400

    commit of home page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { type AppType } from "next/app";
+import '../styles/globals.css'
+
+const MyApp: AppType = ({Component, pageProps}) => {
+  return (
+   <Component {...pageProps} />
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const HomePage = () =>{
+  return (
+    <div>HELLO ERIC</div>
+  )
+}
+
+export default HomePage
